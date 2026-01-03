bdb06918d4e61f8fc519b22404e66f9a29e6da57
sevara128@icloud.com <sevara128@icloud.com>
2026-01-03 19:16:15 +0300

    tortinchi commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
 import { Canvas,useFrame } from '@react-three/fiber'
 import{useRef} from 'react'
 
-import{OrbitControls } from '@react-three/drei';
-
+import{OrbitControls,GizmoHelper,GizmoViewcube,GizmoViewport } from '@react-three/drei';
+import{useControls} from 'leva';
 
 const AnimatedBox = ()=>{
   const boxRef = useRef();
 
+
+  const {color, speed } = useControls({
+
+    color:"#00bfff",
+    speed:{
+      value:0.005,
+      min:0.0,
+      max:0.03,
+      step:0.001
+    }
+  })
+   
+
+
+
   useFrame(()=>{
-    boxRef.current.rotation.x += 0.005;
-    boxRef.current.rotation.y += 0.005;
-    boxRef.current.rotation.z += 0.005;
+    boxRef.current.rotation.x += speed;
+    boxRef.current.rotation.y += speed;
+    boxRef.current.rotation.z += speed;
   });
 
 
@@ -18,7 +33,7 @@ const AnimatedBox = ()=>{
   return(
     <mesh ref={boxRef} >
          <boxGeometry args={[2,3,2]}/>
-         <meshStandardMaterial color={0x00bfff}/>
+         <meshStandardMaterial color={color}/>
          <axesHelper args={[10]}/>
        </mesh>
   )
@@ -28,11 +43,14 @@ const App = () => {
   return (
     <div id='canvas-container'>
       <Canvas>
+        <GizmoHelper aligment='bottom-right' margin={[80,80]}>
+            <GizmoViewport/>
+        </GizmoHelper>
         <gridHelper args={[20,20,0xff22aa,0x55ccff]}/>
         <axesHelper args={[10]}/>
         <OrbitControls />
-          <AnimatedBox/>
-         <directionalLight position={[2,5,1]}/>
+        <AnimatedBox/>
+        <directionalLight position={[2,5,1]}/>
       </Canvas>
     </div>
     
